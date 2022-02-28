7e946e26315a5a8da2c63bda6ba18b1747e8b318
Alexander18101995 <alekud18@yandex.ru>
2022-02-28 20:32:38 +0300

    v2

--- homework.sql
+++ homework.sql
@@ -2,27 +2,30 @@ create table Artist
 (
   Artist_id int primary key,
   Name varchar (50),
-  Genre_id int not null
+  Genre_id int references Genre(Genre_name)
 );
 
 create table Albums
 (
+  Albums_id int primary key,
   Artist_id int references Artist(Artist_id),
-  Albums_id int not null,
   Album_name varchar (50),
   Years integer not null
 );
 
 create table Tracks
 (
+  Track_id int primary key,
   Albums_id int references Albums(Albums_id),
-  Track_id int references Artist(Artist_id),
   Title varchar (50),
-  Times int
+  Times int not null
 );
 
 create table Genre 
 (
-  Genre_id int references Artist(Artist_id),
+  Genre_id int primary key,
   Genre_name varchar(50)
 );
+
+
+
