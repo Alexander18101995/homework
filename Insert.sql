2f76d7e3d2f7dc6d1cb9de9ea1144fb843a0953f
Alexander18101995 <91375108+Alexander18101995@users.noreply.github.com>
2022-03-26 13:03:42 +0300

    Update Insert.sql

--- Insert.sql
+++ Insert.sql
@@ -30,21 +30,21 @@ values
 
 insert into Tracks ( Tracks_id, Albums_id,Title,Times)
 values 
-(1,  2,  'Com',    1,36),
-(2,  1,  'Kri',    2,34),
-(3,  3,  'Kir',    3,00),
-(4,  4,  'Eln',    2,55),
-(5,  2,  'Pars',   1,03),
-(6,  3,  'My sun', 2,43),
-(7,  1,  'Dad',    3,51),
-(8,  7,  'Prek',   2,46),
-(9,  4,  'Kitch',  2,31),
-(10, 5,  'Itchen', 1,59),
-(11, 2,  'Pro',    2,53),
-(12, 8,  'Svo',    1,45),
-(13, 7,  'Dmo',    2,43),
-(14, 6,  'Vnk',    1,45),
-(15, 5,  'Zhuk',   2,32);
+(1,  2,  'Com',    1.36),
+(2,  1,  'Kri',    2.34),
+(3,  3,  'Kir',    3.00),
+(4,  4,  'Eln',    2.55),
+(5,  2,  'Pars',   1.03),
+(6,  3,  'My sun', 2.43),
+(7,  1,  'Dad',    3.51),
+(8,  7,  'Prek',   2.46),
+(9,  4,  'Kitch',  2.31),
+(10, 5,  'Itchen', 1.59),
+(11, 2,  'Pro',    2.53),
+(12, 8,  'Svo',    1.45),
+(13, 7,  'Dmo',    2.43),
+(14, 6,  'Vnk',    1.45),
+(15, 5,  'Zhuk',   2.32);
 
 insert into Collection ( Collection_id, Collection_name,Years)
 values 
