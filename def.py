d02a9f63b9e0f098b0929aa3d1cbee0e7a766dce
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-04-16 18:51:51 +0300

    Add files via upload

--- def.py
+++ def.py
@@ -1,7 +1,7 @@
 documents = [
-        {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-        {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-        {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+ {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+ {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+ {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
       ]
 
 directories = {
@@ -9,34 +9,45 @@ directories = {
         '2': ['10006'],
         '3': []
       }
-
-comand = input('Введите команду: ')
-if comand == 'p':
- def people(number):
+def people(number):
   for doc in documents:
-   if number == doc['number']:
-    print(doc['name'])  
- people(input('Введите номер: '))          
-elif comand == 'l':
-  def list(l):
-    for doc in documents:
-      print(doc['type'], doc['number'], doc['name'])     
-  list(documents)
-elif comand == 's':
- def shelf(s):
+    if number == doc['number']:
+      print(doc['name']) 
+      
+def list(l):
+  for doc in documents:
+    print(doc['type'], doc['number'], doc['name']) 
+
+def shelf(s):
   for dir in directories.items():
     if s in dir[1]:
       print(dir[0]) 
- shelf(input("Введитe номер документа: "))   
-elif comand == 'a':
- def add(a):
-  if a[3] <= '3':
-   documents.append({"type": a[0], "number": a[1], "name": a[2]})
-   directories[a[3]].append(a[1])
-  elif a[3] > '3':
-    directories[a[3]] = []
-    documents.append({"type": a[0], "number": a[1], "name": a[2]})
-    directories[a[3]].append(a[1])
-  print(documents)
-  print(directories)
- add(list(input("Тип, номер, имя, полку(Через запятую)  ").split(", ")))
+
+def add(a):
+  if a[3] in directories.keys():
+        documents.append({"type": a[0], "number": a[1], "name": a[2]})
+        directories[a[3]].append(a[1])
+        print(documents)
+        print(directories)
+  elif a[3] not in directories.keys():
+        directories[a[3]] = []
+        documents.append({"type": a[0], "number": a[1], "name": a[2]})
+        directories[a[3]].append(a[1]) 
+        print(documents)
+        print(directories)
+        
+def main():
+  while True:
+    team = input('Введите команду: ')
+    if team == 'p':
+      print(people(input('Введите номер: ')))
+    elif team == 'l':
+      print(list(documents))
+    elif team == 's':
+      print(shelf(input("Введитe номер документа: ")))
+    elif team == 'a':
+      print(add(input("Тип, номер, имя, полку(Через запятую) ").split(", ")))
+    elif team == 'e':
+      print('До свидания!')
+      break                
+main()
